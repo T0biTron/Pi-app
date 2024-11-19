4fda2e47a28040326cbe16400a3125d5ff2f2251
Santiago Gonzalez <104991113+SantiagoGonzalez03@users.noreply.github.com>
2024-11-19 17:36:37 -0500

    Cambie las 2 de record y el form

--- lib/record_detail_screen.dart
+++ lib/record_detail_screen.dart
@@ -6,14 +6,25 @@ class RecordDetailScreen extends StatelessWidget {
   const RecordDetailScreen({Key? key, required this.record}) : super(key: key);
 
   // Función para calcular la edad a partir de la fecha de nacimiento
-  String calculateAge(String birthdate) {
+  String calculateAge(String birthdate, String testDate) {
     DateTime birthDate = DateTime.parse(birthdate);
-    DateTime today = DateTime.now();
-    int age = today.year - birthDate.year;
-    if (today.month < birthDate.month || (today.month == birthDate.month && today.day < birthDate.day)) {
-      age--;
+    DateTime testDateTime = DateTime.parse(testDate);
+
+    int years = testDateTime.year - birthDate.year;
+    int months = testDateTime.month - birthDate.month;
+    int days = testDateTime.day - birthDate.day;
+
+    if (days < 0) {
+      months--;
+      days += DateTime(testDateTime.year, testDateTime.month, 0).day;
     }
-    return age.toString();
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    return '$years años, $months meses, $days días';
   }
 
   @override
@@ -30,14 +41,14 @@ class RecordDetailScreen extends StatelessWidget {
             _buildInfoRow('Nombre:', record['name']),
             _buildInfoRow('Género:', record['gender']),
             _buildInfoRow('Fecha de Nacimiento:', record['birthdate']),
-            _buildInfoRow('Edad:', calculateAge(record['birthdate'])),
             _buildInfoRow('Fecha del Test:', record['testDate']),
+            _buildInfoRow('Edad:', calculateAge(record['birthdate'], record['testDate'])),
             _buildInfoRow('Mano Preferida:', record['preferredHand'] ?? 'No especificado'),
             _buildInfoRow('Pie Preferido:', record['preferredFoot'] ?? 'No especificado'),
-            _buildInfoRow('Puntaje Locomotora:', record['totalLocomotoraScore'].toString()),
-            _buildInfoRow('Puntaje Pelota:', record['totalPelotaScore'].toString()),
             _buildInfoRow('Nombre del Examinador:', record['examinerName'] ?? 'Desconocido'),
             _buildInfoRow('Título del Examinador:', record['examinerTitle'] ?? 'Desconocido'),
+            _buildInfoRow('Puntaje Locomotora:', record['totalLocomotoraScore'].toString()),
+            _buildInfoRow('Puntaje Pelota:', record['totalPelotaScore'].toString()),
           ],
         ),
       ),
